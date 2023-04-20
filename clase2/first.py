1a4f79534d0f868bcf382005700d6844e77cbfb3
Cloooo <claudia.rojasgalaz@gmail.com>
2023-04-19 22:12:18 -0400

    firstcommit

---
+++ clase2/first.py
@@ -0,0 +1,12 @@
+print("Hello world")
+edad = 40
+sueldo = 5000.3
+nombre = "Claudia"
+isTeacher = False
+
+print(edad, sueldo, nombre, isTeacher)
+
+numero1 = 4
+numero2 = 7
+resultado = (numero1 + numero2) / 3
+print(resultado)
