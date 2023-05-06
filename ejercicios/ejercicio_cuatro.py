df6e3438e9a7c747f899d6591cbba1b55ce5fdb4
Cloooo <claudia.rojasgalaz@gmail.com>
2023-05-05 21:56:33 -0400

    create ejercicio_cuatro.py

---
+++ ejercicios/ejercicio_cuatro.py
@@ -0,0 +1,6 @@
+horas_trabajadas = float(input('Ingrese cantidad de horas trabajadas'))
+valor_hora = float(input('Ingrese el valor por hora'))
+
+pago = horas_trabajadas + valor_hora
+
+print (' El monto a pagar por horas trabajadas es', pago)
