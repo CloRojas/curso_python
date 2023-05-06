24118fdc4c914dd8d27cb7fa643bf429f1099801
Cloooo <claudia.rojasgalaz@gmail.com>
2023-05-05 22:09:22 -0400

    create ejercicio_cinco.py

---
+++ ejercicios/ejercicio_cinco.py
@@ -0,0 +1,7 @@
+cantidad = float(input('Ingrese cantidad invertir'))
+interes = float(input('Ingrese la tasa de interes anual'))
+años = int(input('Ingrese número de años a invertir'))
+
+capital = cantidad * interes * años
+
+print('El monto obtenido de la inversión es', capital)
